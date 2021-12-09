f89e19abece6a1d30e9ccafc8e5fb5e77c36b4f0
fangpeng0011 <fangpeng0011@163.com>
2021-12-09 15:31:16 +0800

    初始化

---
+++ base-java-code/src/main/java/com/ffp/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ffp.test;
+
+/**
+ * @description:
+ * @author: FFP
+ * @time: 2021/12/9 15:30
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
