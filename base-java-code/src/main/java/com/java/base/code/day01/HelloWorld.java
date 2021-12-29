7a0b043d33fc33d8b280d08de80fba93c8b98fc6
fangpeng0011 <fangpeng0011@163.com>
2021-12-29 10:12:31 +0800

    feat(All): 新增模块
    新增Hello World模块

---
+++ base-java-code/src/main/java/com/java/base/code/day01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.base.code.day01;
+
+/**
+ * @description:
+ * @author: FFP
+ * @time: 2021/12/29 10:10
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
